66ba60613749db4d4e3d68fbd050041278d5d84b
Govnor Payne <govnorpayne@gmail.com>
2022-03-03 08:26:27 -0600

    added highscore js

--- assets/js/gameover.js
+++ assets/js/gameover.js
@@ -1,5 +1,21 @@
 window.onload = function () {
   var highscore = localStorage.getItem("mostRecentScore");
-  var finalScore = document.querySelector("finalScore");
-  console.log(highscore);
+  var finalScore = document.querySelector("#finalScore");
+  finalScore.innerText = highscore;
 };
+
+function saveHighScore(event) {
+  event.preventDefault();
+  var highscore = localStorage.getItem("mostRecentScore");
+  var username = document.querySelector("#username");
+  let scores = { [username.value]: highscore };
+  var allscores = localStorage.getItem("scores");
+  if (allscores) {
+    var pushscores = JSON.parse(allscores);
+    pushscores.push(scores);
+    localStorage.setItem("scores", JSON.stringify(pushscores));
+  } else {
+    localStorage.setItem("scores", JSON.stringify([scores]));
+  }
+  console.log(scores);
+}
